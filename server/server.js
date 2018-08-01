bf6645792077a0b800e9e99f354dee914bbff061
Peter Kang <peter.kang@nuance.com>
2018-07-31 19:10:45 -0700

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+const { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
@@ -38,6 +39,19 @@ app.get("/todos", (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) return console.log(`Todo not found`);
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
