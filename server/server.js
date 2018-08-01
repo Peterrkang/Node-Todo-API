70af45cfa032b27d18ed49871a605bf6c5b651c3
Peter Kang <Peterrkang@gmail.com>
2018-08-01 00:12:44 -0700

    Add patch /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 const { ObjectID } = require("mongodb");
+
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
@@ -69,11 +70,29 @@ app.delete("/todos/:id", (req, res) => {
     .catch(e => {
       res.status(400).send();
     });
+});
 
-  //remove todo by id
-  //if err return 400 wit empty
-  //if no doc return 200
-  //
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) return res.status(404).send();
+      return res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 
 app.listen(port, () => {
