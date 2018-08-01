c23278b909bc2916fc97700dcd0753b79b4b73a8
Peter Kang <Peterrkang@gmail.com>
2018-07-31 22:55:31 -0700

    Update process env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost:27017/TodoApp"
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
 );
 
 module.exports = {
