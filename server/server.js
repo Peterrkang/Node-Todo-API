34aa406048110dbcffa75e2ab11940a46673eeca
Peter Kang <Peterrkang@gmail.com>
2018-07-31 23:27:21 -0700

    Add delete todos/:id route

--- server/server.js
+++ server/server.js
@@ -56,6 +56,26 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
+app.delete("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) return res.status(404).send();
+      res.send(todo);
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+
+  //remove todo by id
+  //if err return 400 wit empty
+  //if no doc return 200
+  //
+});
+
 app.listen(port, () => {
   console.log(`Server started on Port: ${port}`);
 });
