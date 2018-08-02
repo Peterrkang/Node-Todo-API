bff84b98fde6966071eeb59bff59ad9b5997bf2f
Peter Kang <peter.kang@nuance.com>
2018-08-01 23:30:48 -0700

    Add POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,46 @@
-var { mongoose } = require("../db/mongoose");
+const { mongoose } = require("../db/mongoose");
+const validator = require("validator");
+
+/*
+  {
+    email: 'kjasld@gmail.com',
+    password: 'bcrypt algo',
+    tokens: [{
+      access: 'auth'
+      token: 'encrypted string'
+    }]
+  }
+*/
 
 var User = mongoose.model("User", {
   email: {
     type: String,
     require: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  token: [
+    {
+      access: {
+        type: String,
+        require: true
+      },
+      token: {
+        type: String,
+        require: true
+      }
+    }
+  ]
 });
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.delete("/todos/:id", (req, res) => {
   Todo.findByIdAndRemove(id)
     .then(todo => {
       if (!todo) return res.status(404).send();
-      res.send(todo);
+      res.send({ todo });
     })
     .catch(e => {
       res.status(400).send();
@@ -93,6 +93,21 @@ app.patch("/todos/:id", (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
+
+//POST /users
+app.post("/user", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  var user = new User(body);
+
+  user.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send();
+    }
+  );
 });
 
 app.listen(port, () => {
