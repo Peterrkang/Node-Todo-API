ea5a1efab854bb1001b0de1ecbb6854393885a3f
Peter Kang <Peterrkang@gmail.com>
2018-08-12 15:30:59 -0700

    add test for DELETE /todos/:id and update route with async/await

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.delete("/todos/:id", async (req, res) => {
     if (!todo) return res.status(404).send();
     res.status(200).send({ todo });
   } catch (e) {
-    res.status(400).send();
+    res.status(400).send(e);
   }
 });
 
