bd82177e62957ad180190b93b1cefec009e88b34
Peter Kang <peter.kang@nuance.com>
2018-08-13 15:57:33 -0700

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require("./config/config.js");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
@@ -7,6 +8,7 @@ const { ObjectID } = require("mongodb");
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
+const { authenticate } = require("./middleware/authenticate");
 const port = process.env.PORT;
 
 //save new something
@@ -104,6 +106,10 @@ app.post("/users", (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
