a44a6f677358552909d705b01eeadb46ad14c3b8
Peter Kang <peter.kang@nuance.com>
2018-08-14 10:22:41 -0700

    Hash user password before save

---
+++ playground/hashing.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+var password = "123abc!";
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword =
+  "$2a$10$SDt6Abx4x3dVLUFwEVwM8e7Ts2pIMUCHpZbPJ.Ol0xRj2dey1eU8q";
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
